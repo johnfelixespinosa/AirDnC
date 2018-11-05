6555404ec86a7524e6ae11bd1e9939ecf03c410a
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-05 15:22:40 -0500

    Listings can be edited and destroyed, before_action #find_listing added

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,11 @@
 class ListingsController < ApplicationController
+  before_action :find_listing, only: [:show, :edit, :update, :destroy]
+
   def index
     @listings = Listing.all
   end
 
   def show
-    @listing = Listing.find_by(id: params[:id])
   end
 
   def new
@@ -20,8 +21,28 @@ class ListingsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @listing.update(listing_params)
+      redirect_to listing_path(@listing)
+    else
+      redirect_to edit_listing_path
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to root_path
+  end
   
   private
+
+    def find_listing
+      @listing = Listing.find_by(id: params[:id])    
+    end
 
     def listing_params
       params.require(:listing).permit(
