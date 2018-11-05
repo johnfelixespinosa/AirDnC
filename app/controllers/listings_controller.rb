652edd20bcabb78813d08f31e2727d21853e756f
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-05 14:53:13 -0500

    Can create new listings, listings#index shows listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,33 @@
 class ListingsController < ApplicationController
   def index
+    @listings = Listing.all
   end
 
   def show
+    @listing = Listing.find_by(id: params[:id])
   end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def listing_params
+      params.require(:listing).permit(
+        :name,
+        :description,
+      )
+    end
+
 end
