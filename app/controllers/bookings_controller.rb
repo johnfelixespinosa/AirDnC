efbf0991cafa6d135e820a587c3fcf88949c0c01
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-09 14:07:22 -0500

    Users can book listings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,10 +4,14 @@ class BookingsController < ApplicationController
   def create
     listing = Listing.find_by(id: params[:listing_id])
 
-    @booking = current_user.bookings.build
+    @booking = current_user.bookings.build(booking_params)
+
+    @booking.listing = listing
+    @booking.price = listing.price
+    @booking.save
 
     if @booking.save
-      redirect_to listing
+      redirect_to user_path(current_user)
     else
       render 'new'
     end
@@ -16,7 +20,10 @@ class BookingsController < ApplicationController
 
   private
     def booking_params
-      params.require(:booking).permit(:price)
+      params.require(:booking).permit(
+        :price,
+        :user_id
+        )
     end
 
   end
